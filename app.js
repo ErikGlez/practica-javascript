62cc40d25d138d44dbadca79e30c4c69f33144d1
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-08-08 19:45:50 -0700

    funcionalidad, obtener y borrar tareas

--- app.js
+++ app.js
@@ -21,11 +21,46 @@ function guardarTarea(e){
    tareas.push(tarea);
    localStorage.setItem('tareas', JSON.stringify(tareas));
   }
-  
 
- 
- 
- 
- 
-  e.preventDefault();
+  getTareas();
+  document.getElementById('formTareas').reset();
+   e.preventDefault();
 }
+
+function getTareas() {
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    let vistaTareas = document.getElementById('tareas');
+
+    vistaTareas.innerHTML = '';
+
+    for (let i = 0; i < tareas.length; i++) {
+    
+      
+        let titulo = tareas[i].titulo;
+        let descripcion = tareas[i].descripcion;
+
+        vistaTareas.innerHTML += `<div class="card mb-3">
+      <div class="card-body">
+       <p>${titulo} - ${descripcion}</p>
+       <a class="btn btn-danger" onclick="borrarTarea('${titulo}')">
+       Borrar
+       </a>
+      </div>
+     </div>`
+     
+    }
+}
+
+function borrarTarea(titulo){ 
+  let tareas =  JSON.parse(localStorage.getItem('tareas'));
+   for(let i =0; i < tareas.length; i++){
+     if(tareas[i].titulo == titulo){
+         tareas.splice(i, 1);
+     }
+
+   }
+   localStorage.setItem('tareas', JSON.stringify(tareas));
+   getTareas();
+}
+
+getTareas();
